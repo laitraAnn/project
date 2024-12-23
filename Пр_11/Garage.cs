26720c55fb6247a2ea6c88649a7abbcb8a46204d
laitraAnn <162510014+laitraAnn@users.noreply.github.com>
2024-12-23 06:54:40 +0300

    commit

---
+++ Пр_11/Garage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+
+namespace ProgForTest
+{
+    public class Garage
+    {
+        private List<Car> cars;
+
+        public Garage()
+        {
+            cars = new List<Car>();
+        }
+
+        public void AddCar(Car car)
+        {
+            cars.Add(car);
+            Console.WriteLine($"\nМашина добавлена: {car}");
+        }
+
+        public void RemoveCar(Car car)
+        {
+            if (cars.Remove(car))
+            {
+                Console.WriteLine($"\nУдалена машина: {car}");
+            }
+            else
+            {
+                Console.WriteLine("\nМашина не найдена.");
+            }
+        }
+
+        public List<Car> SearchCar(string searchTerm)
+        {
+            return cars.Where(c => c.Make.Contains(searchTerm) ||
+                                        c.Model.Contains(searchTerm)).ToList();
+        }
+
+        
+        public void DisplayCars()
+        {
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("\nГараж пуст.");
+            }
+            else
+            {
+                Console.WriteLine("\nМашин в гараже:");
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
---
+++ Пр_11/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgForTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string make = "", model = "";
+            int year;
+            decimal mileage;
+            string command;
+            Garage garage = new Garage();
+
+            Car car1 = new Car("Toyota", "Camry", 2020, 15000);
+            Car car2 = new Car("Honda", "Civic", 2019, 20000);
+            Car car3 = new Car("Ford", "Mustang", 2021, 5000);
+            Car car4 = new Car("Honda", "Mustуауang", 2021, 5000);
+
+            garage.AddCar(car1);
+            garage.AddCar(car2);
+            garage.AddCar(car3);
+            garage.AddCar(car4);
+
+            Console.Clear();
+            do
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Добавить автомобиль");
+                Console.WriteLine("2. Удалить автомобиль");
+                Console.WriteLine("3. Найти автомобиль");
+                Console.WriteLine("4. Показать все автомобили");
+                Console.WriteLine("0. Выход");
+                Console.Write("Ваше действие (введите цифру): ");
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "1":
+                        Console.Write("Введите производителя: ");
+                        make = Console.ReadLine();
+                        Console.Write("Введите модель: ");
+                        model = Console.ReadLine();
+                        Console.Write("Введите год выпуска: ");
+                        while (!int.TryParse(Console.ReadLine(), out year)){
+                            Console.WriteLine("Неверная команда. Пожалуйста, попробуйте снова.");
+                            Console.Write("Введите год выпуска: ");
+                        }
+                        Console.Write("Введите пробег: ");
+                        
+                        
+                        while (!decimal.TryParse(Console.ReadLine(),out mileage))
+                        {
+                            Console.WriteLine("Неверная команда. Пожалуйста, попробуйте снова.");
+                            Console.Write("Введите пробег: ");
+                        }
+                        
+                        Car newCar = new Car(make, model, year, mileage);
+                        garage.AddCar(newCar);
+                        break;
+                    case "2":
+                        
+                        Console.Write("Введите производителя автомобиля для удаления: ");
+                        make = Console.ReadLine();
+                        Console.Write("Введите модель автомобиля для удаления: ");
+                        model = Console.ReadLine();
+
+                        var carToRemove = garage.SearchCar(make).FirstOrDefault(c => c.Model.Equals(model, StringComparison.OrdinalIgnoreCase));
+
+                        if (carToRemove != null)
+                        {
+                            garage.RemoveCar(carToRemove);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Автомобиль не найден.");
+                        }
+                        break;
+                    case "3":
+                        Console.Write("\nКакую машину вы ищите (введите модель или производителя): ");
+                        var searchResults = garage.SearchCar(Console.ReadLine());
+                        foreach (var car in searchResults)
+                        {
+                            Console.WriteLine(car);
+                        }
+                        break;
+                    case "4":
+                        garage.DisplayCars();
+                        break;
+                    case "0":
+                        Console.WriteLine("Выход из программы.");
+                        break;
+                    default:
+                        Console.WriteLine("Неверная команда. Пожалуйста, попробуйте снова.");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+            } while (command != "0");
+        }
+    }
+    }
